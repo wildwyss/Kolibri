087144319b1b1e947e81e798133b9162bb7c068f
Dierk Koenig <dierk.koenig@canoo.com>
2023-06-26 17:46:37 +0200

    examples: intersperse, iterateAsync (still room for improvement)

--- contrib/wild_wyss/src/a_example/exampleTest.js
+++ contrib/wild_wyss/src/a_example/exampleTest.js
@@ -1,10 +1,17 @@
 import {TestSuite} from "../test/test.js";
 import {Range}     from "../range/range.js";
-import {map}      from "../sequence/operators/map/map.js";
+import {map}       from "../sequence/operators/map/map.js";
 import {head}      from "../sequence/terminalOperations/head/head.js";
 
-import {from}      from "../jinq/jinq.js";
-import {retainAll} from "../sequence/operators/retainAll/retainAll.js";
+import {from}         from "../jinq/jinq.js";
+import {retainAll}    from "../sequence/operators/retainAll/retainAll.js";
+import {Sequence}     from "../sequence/constructors/sequence/Sequence.js";
+import {take}         from "../sequence/operators/take/take.js";
+import {isEmpty}      from "../sequence/terminalOperations/isEmpty/isEmpty.js";
+import {drop}         from "../sequence/operators/drop/drop.js";
+import {PureSequence} from "../sequence/constructors/pureSequence/pureSequence.js";
+import {cons}         from "../sequence/operators/cons/cons.js";
+import {nil}          from "../sequence/constructors/nil/nil.js";
 
 const exampleSuite = TestSuite("Example Suite");
 
@@ -48,14 +55,51 @@ exampleSuite.add("Pythagorean Triples 3", assert => {
 
     const result =
         retainAll( ([x,y,z]) => x*x + y*y === z*z ) (
-            Range(1, Number.MAX_VALUE).and ( z =>
-                Range(1, z).and (y =>
-                    map (x => [x,y,z]) (Range(1,y))
-               )
-           )
-        );
+            Range(1, Number.MAX_VALUE)
+            .and ( z => Range(1, z)
+            .and ( y => map (x => [x,y,z]) (Range(1,y))
+        )));
 
     assert.iterableEq([3,4,5], /** @type { Iterable } */ head (result));
+});
+
+exampleSuite.add("intersperse", assert => {
+
+    const intersperse = inner => sequence => drop (1) (sequence.and( value => cons (inner) (PureSequence(value))));
+
+    assert.iterableEq( nil,                     intersperse( 42) (nil));
+    assert.iterableEq([0],                      intersperse( 42) (Range(0)));
+    assert.iterableEq([0, 42, 1, 42, 2, 42, 3], intersperse( 42) (Range(3)));
+});
+
+exampleSuite.add("Stairs", assert => {
+    const Point = (x,y)     => ({x,y});
+    const Quad  = (a,b,c,d) => ({a,b,c,d});
+    const start = Quad( Point(10,10), Point(10,-10), Point(-10,-10), Point(-10,10) );
+    const forever = _p => true;
+    const produce = ({a,b,c,d}) => Quad( b, c, d, Point( d.x + (a.x-d.x)*1.05, d.y + (a.y-d.y)*1.05));
+    const doodle = Sequence(start, forever, produce);
+
+    assert.is(head(doodle).a.x, 10);
+
+    const iterateAsync = executorSequence => {
+        if (isEmpty(executorSequence)) return;
+        // noinspection JSCheckFunctionSignatures // it appears that ExecutorFunction is not known as the Promise Parameter Type
+        new Promise(head(executorSequence)).then( _ => iterateAsync(drop (1) (executorSequence)));
+    };
+
+    const waitAndLogPoint = ({a}) =>
+        (resolve, _reject) =>
+            setTimeout( _=> {
+                console.log(a.x, a.y);
+                resolve();
+            }, 1000)
+        ;
+    const slowDoodle = map (waitAndLogPoint) (doodle);
+
+    console.log("--------- asynchronous ");
+    iterateAsync (take (10) (slowDoodle));
+
 });
 
 exampleSuite.run();
